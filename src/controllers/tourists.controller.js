469dac601b8f61c9caa29452bf95986d3f19c64c
Miguel Oña Guerrero <miguelog3295@outlook.com>
2022-10-29 01:14:07 +0200

    Add get duration of tourists

--- src/controllers/tourists.controller.js
+++ src/controllers/tourists.controller.js
@@ -16,3 +16,17 @@ exports.findByComunidadAutonoma = (req, res) => {
     
     res.json(tourists);
 }
+
+exports.findTripDuration = (req, res) => {
+    const { origen, comunidadAutonoma, mes } = req.params;
+
+    var filterByDuration = function(tourist) { return tourist.MetaData[0].Nombre == "Duración media de los viajes" };
+    var filterByComunidadAutonoma = function(tourist) { return tourist.MetaData[3].Nombre == comunidadAutonoma };
+    var filterByOrigen = function(tourist) { return tourist.MetaData[1].Nombre == origen };
+    var filterByFecha = function(month) { return month.T3_Periodo == mes && month.Anyo == 2021 };
+
+    const duration = Tourists.filter(filterByDuration).filter(filterByComunidadAutonoma).filter(filterByOrigen)[0]
+                        .Data.filter(filterByFecha)[0].Valor;
+
+    res.json(duration);
+} 
